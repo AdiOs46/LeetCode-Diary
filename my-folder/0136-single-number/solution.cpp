d2e7ebb240653c68f993920ea176ec099ed5156d
Aditya Ojha <32695552+AdiOs46@users.noreply.github.com>
2024-06-01 10:23:52 +0000

    [LeetCode Sync] Runtime - 15 ms (51.93%), Memory - 19.3 MB (57.25%)

--- my-folder/0136-single-number/solution.cpp
+++ my-folder/0136-single-number/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int singleNumber(vector<int> nums) { 
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i+=2) {
-            if(nums[i] != nums[i+1]) return nums[i];
+    int singleNumber(vector<int>& nums) { 
+        int num=0;
+        for(int i=0; i<nums.size(); i++) {
+            num = num ^ nums[i];
         }
-        return nums.back();
+        return num;
     }
 };
