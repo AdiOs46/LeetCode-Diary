4011ac7c4c839623ab66d45fe7575c5a6b5506b9
Aditya Ojha <32695552+AdiOs46@users.noreply.github.com>
2024-06-21 17:10:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.9 MB (46.83%)

--- my-folder/0144-binary-tree-preorder-traversal/solution.cpp
+++ my-folder/0144-binary-tree-preorder-traversal/solution.cpp
@@ -13,14 +13,16 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        preorder(root, ans);
+        if (root == NULL) return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()) {
+            TreeNode* curr = st.top();
+            ans.push_back(curr->val);
+            st.pop();
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+        }
         return ans;
-    }
-
-    void preorder(TreeNode* root, vector<int>& ans) {
-        if(root == NULL) return;
-        ans.push_back(root->val);
-        preorder(root->left, ans);
-        preorder(root->right, ans);
     }
 };
