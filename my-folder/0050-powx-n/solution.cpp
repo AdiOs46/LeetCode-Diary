a87e816f5822d031efe7ec69a204c6734011f4ba
Aditya Ojha <32695552+AdiOs46@users.noreply.github.com>
2024-06-29 17:54:36 +0000

    [LeetCode Sync] Runtime - 25 ms (20.84%), Memory - 37.7 MB (10.78%)

--- my-folder/0050-powx-n/solution.cpp
+++ my-folder/0050-powx-n/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+         if (n == INT_MAX) 
+            return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+        if (n == INT_MIN) 
+            return (x == 1 || x == -1) ? 1 : 0;
+        
         double ans = 1;
-        if(x == 0 || x == 1) return x;
-
-        if(n < 0){
-            x = 1/x;
-            n = -(n+1);
-            ans = ans * x;
-        }
-
-        while(n>0) {
-            if(!(n&1)) {
-                n = n/2;
-                x = x * x;
-            } else {
-                ans *= x;
-                n = n-1;
-            }
-        }
+        if(n < 0) {
+            n = -n;
+            ans = pow(x, n);
+            ans = 1.0/ans;
+        }else ans = pow(x, n);
+        
         return ans;
+    }
+
+    double pow(double x, int n) {
+        if(n <= 0) return 1;
+        return x * pow(x, n-1);
     }
 };
