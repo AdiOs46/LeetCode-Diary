79577a17ea0c389599da8f7717c7f91fe793f4a2
Aditya Ojha <32695552+AdiOs46@users.noreply.github.com>
2024-06-29 09:40:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.2 MB (37.08%)

--- my-folder/0231-power-of-two/solution.cpp
+++ my-folder/0231-power-of-two/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        if (!(n & (n-1))) return true;
-        return false;
+        if(n == 1) return true;
+        if(n == 0) return false;
+        return n%2 == 0 && isPowerOfTwo(n/2);
     }
 };
