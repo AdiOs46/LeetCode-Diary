04b82f3ed2e95ec232745362cbb089d422dfcb8d
Aditya Ojha <32695552+AdiOs46@users.noreply.github.com>
2024-07-05 10:51:37 +0000

    [LeetCode Sync] Runtime - 8 ms (71.22%), Memory - 21.7 MB (41.78%)

--- my-folder/0110-balanced-binary-tree/solution.cpp
+++ my-folder/0110-balanced-binary-tree/solution.cpp
@@ -12,19 +12,21 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr)
-            return true;
+        if(!root) return true;
+        
         return height(root) != -1;
     }
+
     int height(TreeNode* root){
-        if(root == nullptr){
-            return 0;
-        }
-        int lHeight = height(root->left);
-        int rHeight = height(root->right);
-        if(lHeight == -1 || rHeight == -1 || abs(lHeight - rHeight) > 1){
-            return -1;
-        }
-        return 1 + max(lHeight, rHeight);
+        if(!root) return 0;
+
+        int countL = height(root->left);
+        if (countL == -1) return -1;
+
+        int countR = height(root->right);
+        if (countR == -1) return -1;
+
+        if(abs(countL - countR) > 1) return -1;
+        return max(countL, countR) + 1;
     }
 };
