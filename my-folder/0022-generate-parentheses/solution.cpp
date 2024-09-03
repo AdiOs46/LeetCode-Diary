63731ac57cb6d82042c4b16088a1582573849c6f
Aditya Ojha <32695552+AdiOs46@users.noreply.github.com>
2024-09-03 12:59:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.4 MB (33.37%)

---
+++ my-folder/0022-generate-parentheses/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void generate(int open, int close, string str, vector<string>& ans) {
+        if(open == 0 && close == 0) {
+            ans.push_back(str);
+            return;
+        }
+
+        if(open > 0) {
+            generate(open-1, close, str + "(", ans);
+        }
+
+        if(close > open) {
+            generate(open, close-1, str + ")", ans);
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        int open = n, close = n;
+        vector<string> ans;
+        generate(n, n, "", ans);
+        return ans;
+    }
+};
